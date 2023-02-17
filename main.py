1baab3841fa7d512e7f1de33f518d196ca8e196b
pedroruas <ruas@rocketmat.com>
2023-02-17 10:30:41 -0500

    Added the call to the function 'separation_index_calculation' in order to compute the Separation Index for each Triadic Concept.

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ from TriadicConcept import TriadicConcept
 from AssociationRules import AssociationRule
 
 
-def triadic_miner(file_path, compute_minimality_for_infimum, compute_concept_stability, save_hasse_diagram, file_name):
-    Timer.start("Reading triadic concepts")
+def triadic_miner(file_path, compute_feature_generators_for_infimum, compute_concept_stability, compute_separation_index , save_hasse_diagram, file_name):
+    
+    Timer.start("Reading Triadic Concepts")
     triadic_concepts = TriadicConcept.get_triadic_concepts_from_input_file(
         file_path)
     print("Number of Triadic Concepts:", len(triadic_concepts))
     Timer.stop()
     
-    Timer.start("Creating triadic concepts faces")
+    Timer.start("Creating Triadic Concepts Faces")
     faces, all_extents = TriadicConcept.create_triadic_concepts_faces(
         triadic_concepts)
     Timer.stop()
@@ -29,7 +30,7 @@ def triadic_miner(file_path, compute_minimality_for_infimum, compute_concept_sta
     Timer.stop()
     
     Timer.start("Computing F-Generators")
-    updated_triadic_concepts = TriadicConcept.compute_f_generators_candidates(triadic_concepts, links, compute_minimality_for_infimum)
+    updated_triadic_concepts = TriadicConcept.compute_f_generators_candidates(triadic_concepts, links, compute_feature_generators_for_infimum)
     Timer.stop()
     
     Timer.start("Computing Formal Context")
@@ -62,10 +63,16 @@ def triadic_miner(file_path, compute_minimality_for_infimum, compute_concept_sta
         updated_triadic_concepts = TriadicConcept.compute_concept_stability(triadic_concepts, formal_context)
         Timer.stop()
     
+    if compute_separation_index:
+        Timer.start("Computing Separation Index")
+        updated_triadic_concepts = TriadicConcept.separation_index_calculation(updated_triadic_concepts)
+        Timer.stop()
+    
     if save_hasse_diagram:
         Timer.start("Creating the Hasse Diagram")
         TriadicConcept.create_hasse_diagram(updated_triadic_concepts, links, file_name)
-        time = Timer.stop()
+        Timer.stop()
+    
     
     # print("TRIADIC CONCEPTS")
     # for c in updated_triadic_concepts:
@@ -105,15 +112,16 @@ def main():
         file_name = file_name.split(".data.out")[0]
         output_dir = data['output_dir']
         print(f'Running Triadic Miner on {input_file_path} file...')
-        compute_minimality_for_infimum = data[
-            'compute_feature_generators_minimality_test_for_infimum']
+        compute_feature_generators_for_infimum = data[
+            'compute_feature_generators_for_infimum']
         compute_concept_stability = data['compute_concept_stability']
+        compute_separation_index = data['compute_separation_index']
         save_hasse_diagram = data[
             'save_hasse_diagram']
         print()
         
         start_time = timeit.default_timer()
-        triadic_miner(input_file_path, compute_minimality_for_infimum, compute_concept_stability , save_hasse_diagram, file_name)
+        triadic_miner(input_file_path, compute_feature_generators_for_infimum, compute_concept_stability, compute_separation_index, save_hasse_diagram, file_name)
         end_time = timeit.default_timer()
         print("TOTAL TIME:  %0.4f SECONDS" % float(end_time - start_time))
 
